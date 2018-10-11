6a5722f20af8218e5efcd2c01369e5f70300d9db
Nandini Nayak <nandini.r.nayak@gmail.com>
2018-10-11 12:34:40 +1100

    first commit

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://nandini:nandini1@ds259802.mlab.com:59802/test-dev",
-  googleClientID:
-    "534881317826-93bh00b7cgpkp73lku8vtjo2fvrrtdi6.apps.googleusercontent.com",
-  googleClientSecret: "CWdrsM_DcaNBG_wY09CxQODZ"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
