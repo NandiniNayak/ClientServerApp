0ad8d8e60ba82f79022d86a342b8732efe9c0f1f
Nandini Nayak <nandini.r.nayak@gmail.com>
2018-10-11 12:30:27 +1100

    first commit

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
-  mongoURI: 'mongodb://nandini:nandini1@ds141621.mlab.com:41621/storyboard-dev',
-  googleClientID: '874100730369-ffp3l6p1m72muqjttpbrfikbna43u19b.apps.googleusercontent.com',
-  googleClientSecret: 'yhaFQdPojpVHMIAiW0wTLRS-'
-}
+  mongoURI: "mongodb://nandini:nandini1@ds259802.mlab.com:59802/test-dev",
+  googleClientID:
+    "534881317826-93bh00b7cgpkp73lku8vtjo2fvrrtdi6.apps.googleusercontent.com",
+  googleClientSecret: "CWdrsM_DcaNBG_wY09CxQODZ"
+};
