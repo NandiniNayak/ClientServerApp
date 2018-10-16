b2f280812468dce6a9d9e5e145250dbaec8f1cdb
Nandini Nayak <nandini.r.nayak@gmail.com>
2018-10-16 16:26:10 +1100

    stripe payment and environment variables set

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -4,13 +4,20 @@ import StripeCheckout from "react-stripe-checkout";
 class Payments extends Component {
   // token s expecting to receive callback function that will be called after receiving suthorised token from stripe
   // add debugger to chcek js code generated at this point
+  // notice the token in console logged
+  // StripeChecout is a component which accepts additional props like name and description
+  // replace stripe button with a custom element as child component: eg: button
   render() {
     return (
       <StripeCheckout
+        name="Emaily"
+        description="$5 for 5 email credits"
         amount={500}
         token={token => console.log(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
-      />
+      >
+        <button className="btn">Add Credits</button>
+      </StripeCheckout>
     );
   }
 }
